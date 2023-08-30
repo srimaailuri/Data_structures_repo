1a5df1400e8c3c5d2706bcd73666188e9ad96bfb
srima <as9731@srmist.edu.in>
2023-08-30 21:27:17 +0530

    sorting using cpp

---
+++ sorting/bubble_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int bubble_sort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+
+void print_arr(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    bubble_sort(arr,n);
+    print_arr(arr,n);
+
+}
